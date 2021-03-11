3935fdd8ccc891b82351b8c29ed9919dfde80af5
Hank <hankchen1997@yahoo.com>
2021-03-11 16:20:29 -0500

    finished backend authentication and user/session models

--- BenchBnB/bench_bnb/app/controllers/api/users_controller.rb
+++ BenchBnB/bench_bnb/app/controllers/api/users_controller.rb
@@ -2,15 +2,20 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      render :show
+    
+    if User.find_by(username: user_params[:username])
+        render json: @user.errors.full_messages, status: 401
+    elsif @user.save
+        
+        render :show
     else
-      render :json @user.errors.full_messages, status: 0
+        render json: @user.errors.full_messages, status: 402
+    end
   end
 
 
   private 
   def user_params
-    params.require(:user).permits(:username, :password)
+    params.require(:user).permit(:username, :password)
   end
 end
