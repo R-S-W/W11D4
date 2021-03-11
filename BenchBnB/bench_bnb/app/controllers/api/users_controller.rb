42672537338f852509cf252111af0676b6f35a94
Ray <raysidarta@gmail.com>
2021-03-11 17:04:38 -0500

    Work on phase1 finished reducers

--- BenchBnB/bench_bnb/app/controllers/api/users_controller.rb
+++ BenchBnB/bench_bnb/app/controllers/api/users_controller.rb
@@ -6,7 +6,7 @@ class Api::UsersController < ApplicationController
     if User.find_by(username: user_params[:username])
         render json: @user.errors.full_messages, status: 401
     elsif @user.save
-        
+        login!(@user)
         render :show
     else
         render json: @user.errors.full_messages, status: 402
